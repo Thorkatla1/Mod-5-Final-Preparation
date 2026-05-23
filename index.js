57a22e3c7bddf208af826dc43036d959d445523e
Thorkatla1 <thorkatla@yahoo.com>
2026-05-22 20:36:04 -0400

    complete userHTML

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-
+console.log(usersData)
   const userListEl = document.querySelector(".user-list");
-  userListEl.innerHTML = usersData  
-    .map(
-      (user) => `<div class="user-card">
-    <div class="user-card__container">
-    <h3>User's Name</h3>
-    <p><b>Email:</b> email@email.com</p>
-    <p><b>Phone:</b> 0000000000</p>
-    <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-</div>
-</div>`,
-    )
-    .join("");
+  userListEl.innerHTML = usersData.map((user)=> userHTML (user)).join("")
 }
 main();
-// 4.25
+function userHTML(user){
+  return `<div class="user-card">
+    <div class="user-card__container">
+      <h3>${user.name}</h3>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+        ${user.website}
+        </a></p>
+    </div>
+  </div>`
+}
