0a72026fcd8f29d350b41d5ba3a98d9288dd9422
Thorkatla1 <thorkatla@yahoo.com>
2026-05-25 02:48:44 -0400

    is the card working?

--- user.js
+++ user.js
@@ -1,28 +1,32 @@
-// 21:09
-
 const postListEl = document.querySelector(".post-list");
-
-function onSearchChange(event){
-    console.log(event.target.value)
+const id = localStorage.getItem("id");
+async function onSearchChange(event) {
+  const id = event.target.value;
+  renderPosts(id)
 }
 
-
-async function main() {
-  const id = localStorage.getItem("id");
-  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+async function renderPosts(id) {
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`,
+  );
   const postsData = await posts.json();
-  console.log(postsData);
+  postListEl.innerHTML = postsData.map(post => postHTML(post)).join("");
+}
 
-  postListEl.innerHTML = postsData.map(post=>
-        `<div class="post">
+function postHTML(post) {
+  return`<div class="post">
         <div class="post__title">
            ${post.title}
         </div>
         <p class="post__body">
             ${post.body}
         </p>
-    </div>`,
-    ).join('');
+    </div>
+    `;
 }
+renderPosts(id);
 
-main();
+
+// event needs to be called 'event' or it won't work
+// first mount = when page loads
+// total explanation on 27:32
